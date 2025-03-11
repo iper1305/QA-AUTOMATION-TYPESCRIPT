c195d9ca235c3d2c0dc79e65e570629202026ad5
irynaperepada <iper1305@gmail.com>
2025-03-11 13:19:47 +0200

    made changes with mock

--- src/homework_13/pet-store-api/tests/provider/ provider-pact.spec.ts
+++ src/homework_13/pet-store-api/tests/provider/ provider-pact.spec.ts
@@ -1,19 +1,24 @@
 import { Verifier } from '@pact-foundation/pact';
 import * as path from 'path';
+import axios from 'axios';
 
 describe('Pact Verification', () => {
     it('validates the expectations of PetStoreClient', async function (this: Mocha.Context) {
-        this.timeout(10000); // Додати таймаут, щоб дати більше часу для підключення
+        this.timeout(10000);
 
         const opts = {
-            providerBaseUrl: 'http://localhost:8080', // URL вашого працюючого API
+            providerBaseUrl: 'https://petstore.swagger.io/v2',
             pactUrls: [path.resolve(__dirname, '../../pacts')],
             provider: 'PetStoreAPI',
             providerVersion: '1.0.0',
             stateHandlers: {
                 'a pet with ID 1 exists': async () => {
-                    // Налаштуйте стан провайдера тут (наприклад, переконайтеся, що pet з ID 1 існує в базі даних)
-                    return Promise.resolve();
+                    const response = await axios.get('https://petstore.swagger.io/v2/pet/1');
+                    if (response.status === 200 && response.data.id === 1) {
+                        return Promise.resolve();
+                    } else {
+                        return Promise.reject(new Error('Pet with ID 1 does not exist'));
+                    }
                 }
             }
         };
